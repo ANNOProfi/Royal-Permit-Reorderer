a107d9f06c9cfd92be49f083c90aa199bf1488ea
ANNOProfi <ANNOProfi@t-online.de>
2024-09-23 22:26:56 +0200

    Cookie cutter initial commit

---
+++ 1.5/Source/RoyalPermitReorderer/RoyalPermitReordererMod.cs
@@ -0,0 +1,34 @@
+﻿using Verse;
+using UnityEngine;
+using HarmonyLib;
+
+namespace RoyalPermitReorderer;
+
+public class RoyalPermitReordererMod : Mod
+{
+    public static Settings settings;
+
+    public RoyalPermitReordererMod(ModContentPack content) : base(content)
+    {
+        Log.Message("Hello world from RoyalPermitReorderer");
+
+        // initialize settings
+        settings = GetSettings<Settings>();
+#if DEBUG
+        Harmony.DEBUG = true;
+#endif
+        Harmony harmony = new Harmony("ANNOProfi.rimworld.RoyalPermitReorderer.main");	
+        harmony.PatchAll();
+    }
+
+    public override void DoSettingsWindowContents(Rect inRect)
+    {
+        base.DoSettingsWindowContents(inRect);
+        settings.DoWindowContents(inRect);
+    }
+
+    public override string SettingsCategory()
+    {
+        return "RoyalPermitReorderer_SettingsCategory".Translate();
+    }
+}
