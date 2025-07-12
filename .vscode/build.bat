858a9fb4ad0f835b333cbfbc2db7d56c152819b8
ANNOProfi <ANNOProfi@t-online.de>
2025-07-12 12:40:08 +0200

    update to 1.6

--- .vscode/build.bat
+++ .vscode/build.bat
@@ -4,5 +4,5 @@ REM remove unnecessary assemblies
 DEL .\*\Assemblies\*.*
 
 REM build dll
-dotnet restore 1.5/Source/RoyalPermitReorderer.sln
-dotnet build 1.5/Source/RoyalPermitReorderer.sln /p:Configuration=Debug
+dotnet restore Source/RoyalPermitReorderer.sln
+dotnet build Source/RoyalPermitReorderer.sln /p:Configuration=Debug
